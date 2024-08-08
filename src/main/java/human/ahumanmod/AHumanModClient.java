348f28dd2d81e59c65b5b5546db92f0c2b21e571
ahumanhuman <wyatt.schwick@gmail.com>
2024-08-08 09:38:21 -0500

    Test

---
+++ src/main/java/human/ahumanmod/AHumanModClient.java
@@ -0,0 +1,12 @@
+package human.ahumanmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AHumanModClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/human/ahumanmod/AHumanModDataGenerator.java
@@ -0,0 +1,11 @@
+package human.ahumanmod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class AHumanModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
